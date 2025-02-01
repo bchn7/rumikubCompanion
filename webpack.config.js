b9916b60bb200a62a706bf1a3a63e35d1f1915bf
bchn7 <bchn77@icloud.com>
2025-02-01 02:17:39 +0100

    changed webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
-// webpack.config.js
 const Encore = require('@symfony/webpack-encore');
 
 Encore
     .setOutputPath('public/build/')
     .setPublicPath('/build')
     .addEntry('app', './assets/app.js')
-    // enable CSS processing
-    .enablePostCssLoader()
-// ... other configurations
+    .enableSingleRuntimeChunk() // Enable the single runtime chunk here
+    .cleanupOutputBeforeBuild()
+    .enableBuildNotifications()
+    .enableSourceMaps(!Encore.isProduction())
+    .enableVersioning(Encore.isProduction())
 ;
 
 module.exports = Encore.getWebpackConfig();
